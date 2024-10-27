57101a57ea633d3b7a0f71e351ab2879352d69da
Sven Seeberg <mail@sven-seeberg.de>
2024-10-27 11:25:34 +0100

    Working implementation

--- GenderStarsApp.ts
+++ GenderStarsApp.ts
@@ -1,14 +1,29 @@
 import { IAppAccessors, ILogger, IConfigurationExtend } from '@rocket.chat/apps-engine/definition/accessors';
 import { App } from '@rocket.chat/apps-engine/definition/App';
 import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
-import { GenderStars } from './GenderStars';
 
-export class GenderStarsApp extends App {
+import { IPreMessageSentModify } from '@rocket.chat/apps-engine/definition/messages';
+import { IMessage } from '@rocket.chat/apps-engine/definition/messages';
+import { IMessageBuilder, IModify, IRead, IHttp, IPersistence } from '@rocket.chat/apps-engine/definition/accessors';
+
+export class GenderStarsApp extends App implements IPreMessageSentModify {
     constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
         super(info, logger, accessors);
     }
 
-//    public async initialize(configuration: IConfigurationExtend): Promise<void> {
-//        await configuration.scheduler.registerProcessors([new GenderStars(this)]);
-//    }
+    async executePreMessageSentModify(
+        message: IMessage,
+        builder: IMessageBuilder,
+        read: IRead,
+        http: IHttp,
+        persistence: IPersistence
+    ):Promise<IMessage>
+    {
+        const pattern = /(\w)\*(in|In)/g;
+        const replacement = '$1\\*$2';
+        if (message.text && pattern.test(message.text)) {
+            message.text = message.text.replace(pattern, replacement);
+        }
+        return message;
+    }
 }
